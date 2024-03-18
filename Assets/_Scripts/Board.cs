4880cf3216fdf3d3f2ca687f68b7a5943100ce6d
btcox4 <BTC55@pitt.edu>
2024-03-18 03:05:35 -0400

    wordle works! (sorta)

--- Assets/_Scripts/Board.cs
+++ Assets/_Scripts/Board.cs
@@ -20,6 +20,12 @@ public class Board : MonoBehaviour
     private int rowIndex;
     private int colIndex;
 
+    public Tile.State emptyState;
+    public Tile.State occupiedState;
+    public Tile.State correctState;
+    public Tile.State wrongSpotState;
+    public Tile.State incorrectState;
+
     private void Awake()
     {
         rows = GetComponentsInChildren<Row>();
@@ -29,11 +35,13 @@ public class Board : MonoBehaviour
     {
         LoadData();
         sol = "phone";
+        rowIndex = 0;
+        colIndex = 0;
     }
 
     private void LoadData()
     {
-        TextAsset textFile = Resources.Load("offical_wordle_all") as TextAsset;
+        TextAsset textFile = Resources.Load("official_wordle_all") as TextAsset;
         valid = textFile.text.Split('\n');
     }
 
@@ -46,12 +54,13 @@ public class Board : MonoBehaviour
         {
             colIndex = Mathf.Max(colIndex - 1, 0);
             curRow.tiles[colIndex].SetLetter('\0');
+            curRow.tiles[colIndex].SetState(emptyState);
         }
         else if (colIndex >= curRow.tiles.Length)
         {
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                // SubmitRow(curRow);
+                SubmitRow(curRow);
             }
         }
         else
@@ -61,6 +70,7 @@ public class Board : MonoBehaviour
                 if (Input.GetKeyDown(SUPPORTED_KEYS[i]))
                 {
                     curRow.tiles[colIndex].SetLetter((char)SUPPORTED_KEYS[i]);
+                    curRow.tiles[colIndex].SetState(occupiedState);
                     colIndex++;
                     break;
                 }
@@ -68,6 +78,124 @@ public class Board : MonoBehaviour
         }
     }
 
+    private void SubmitRow(Row row)
+    {
+        if(!isValid(row.word))
+        {
+            // add screenshake?
+            return;
+        }
+        
+        string remaining = sol;
+        
+        for (int i = 0; i < row.tiles.Length; i++)
+        {
+            Tile tile = row.tiles[i];
+            
+            if (tile.letter == sol[i])
+            {
+                // correct
+                tile.SetState(correctState);
+
+                remaining = remaining.Remove(i, 1);
+                remaining = remaining.Insert(i, " ");
+            }
+            else if (!sol.Contains(tile.letter))
+            {
+                // wrong spot
+                tile.SetState(incorrectState);
+            }
+        }
+
+        for(int j = 0; j < row.tiles.Length; j++)
+            {
+                Tile tile1 = row.tiles[j];
+
+                if(tile1.state != correctState && tile1.state != incorrectState)
+                {
+                    if (remaining.Contains(tile1.letter))
+                    {
+                        tile1.SetState(wrongSpotState);
+                        int index = remaining.IndexOf(tile1.letter);
+                        remaining = remaining.Remove(index, 1);
+                        remaining = remaining.Insert(index, " ");
+                    }
+                    else
+                    {
+                        tile1.SetState(incorrectState);
+                    }
+                }
+            }
+
+        if (hasWon(row))
+        {
+            enabled = false;
+        }
+
+        rowIndex++;
+        colIndex = 0;
+
+        // out of rows
+        if (rowIndex >= rows.Length)
+        {
+           if (!hasWon(row))
+           {
+                Advance();
+           }
+        }
+    }
    
+    private bool isValid(string word)
+    {
+        for (int i = 0; i < valid.Length; i++)
+        {
+            if (valid[i] == word){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool hasWon(Row row)
+    {
+        for (int i = 0; i < row.tiles.Length; i++)
+        {
+            if (row.tiles[i].state != correctState)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void Advance()
+    {
+        // clears first row
+        for (int col = 0; col < rows[0].tiles.Length; col++)
+        {
+            rows[0].tiles[col].SetLetter('\0');
+            rows[0].tiles[col].SetState(emptyState);
+        }
+
+        // loops through rest of rows and moves them up one
+        for (int row = 1; row < rows.Length; row++)
+        {
+            for (int col = 0; col < rows[row].tiles.Length; col++)
+            {
+                rows[row - 1].tiles[col].SetLetter(rows[row].tiles[col].letter);
+                rows[row - 1].tiles[col].SetState(rows[row].tiles[col].state);
+            }
+        }
+
+        // clears last row
+            for (int col = 0; col < rows[6].tiles.Length; col++)
+            {
+                rows[6].tiles[col].SetLetter('\0');
+                rows[6].tiles[col].SetState(emptyState);
+            }
+        
+        colIndex = 0;
+        rowIndex--;
+    }
 
 }
--- Assets/_Scripts/Tile.cs
+++ Assets/_Scripts/Tile.cs
@@ -6,13 +6,24 @@ using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
+    [System.Serializable]
+    public class State
+    {
+        public Color fillColor;
+        public Color outlineColor;
+    }
     private TextMeshProUGUI text;
+    private Image fill;
+    private Outline outline;
 
+    public State state { get; private set; }
     public char letter { get; private set; }
 
     private void Awake()
     {
         text = GetComponentInChildren<TextMeshProUGUI>();
+        fill = GetComponent<Image>();
+        outline = GetComponent<Outline>();
     }
 
     public void SetLetter(char letter)
@@ -20,4 +31,12 @@ public class Tile : MonoBehaviour
         this.letter = letter;
         text.text = letter.ToString();
     }
+
+    public void SetState(State state)
+    {
+        this.state = state;
+        fill.color = state.fillColor;
+        outline.effectColor = state.outlineColor;
+    }
+
 }
